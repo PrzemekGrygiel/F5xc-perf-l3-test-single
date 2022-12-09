6023a0994b584c605a08ff4e3b109adffe4f1f29
Przemyslaw Grygiel <przemyslaw.grygiel@gmail.com>
2022-12-09 17:01:47 +0100

    Create run-test.sh

---
+++ run-test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ulimit -n 1000000
+echo "################## Run 135B #################"; wrk -s responses_code.lua --latency --timeout 60s -t 100 -c 2000 -d 2m https://example.com/135.html -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/99.0.4844.51 Safari/537.36" -H "Accept: */*"| grep -v 0,0,0,0 | tee -a 135.txt &&
+sleep 2
+echo "################# Run 1M #################"; echo 31; wrk -s responses_code.lua --latency --timeout 60s -t 100 -c 650 -d 2m https://example.com/1M.html -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/99.0.4844.51 Safari/537.36" -H "Accept: */*"| grep -v 0,0,0,0 | tee -a 1M.txt &&
+sleep 2
+echo "################# Run MIX #################"
+echo "15k"; wrk -s responses_code.lua --latency --timeout 60s -t 100 -c 650 -d 2m https://example.com/15k.html -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/99.0.4844.51 Safari/537.36" -H "Accept: */*"| grep -v 0,0,0,0 | tee -a 15k.txt &
+echo "37k"; wrk -s responses_code.lua --latency --timeout 60s -t 100 -c 650 -d 2m https://example.com/37k.html -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/99.0.4844.51 Safari/537.36" -H "Accept: */*"| grep -v 0,0,0,0 | tee -a 37k.txt &
+echo "91k"; wrk -s responses_code.lua --latency --timeout 60s -t 100 -c 650 -d 2m https://example.com/91k.html -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/99.0.4844.51 Safari/537.36" -H "Accept: */*"| grep -v 0,0,0,0 | tee -a 91k.txt
