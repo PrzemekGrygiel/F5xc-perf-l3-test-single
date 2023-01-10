f3eb812f602638491274f3efa6732d6a50656eab
Przemyslaw Grygiel <przemyslaw.grygiel@gmail.com>
2023-01-10 22:17:09 +0100

    Update run-test-cps.sh

--- run-test-cps.sh
+++ run-test-cps.sh
@@ -9,9 +9,8 @@ echo "################# Run MIX #################"
 echo "15k"; wrk -s responses_code.lua --latency --timeout 60s -t 100 -c 650 -d 2m http://example.com/15k.html -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/99.0.4844.51 Safari/537.36" -H "Accept: */*" -H "Connection: close"| grep -v 0,0,0 | tee -a 15k.txt &
 echo "37k"; wrk -s responses_code.lua --latency --timeout 60s -t 100 -c 650 -d 2m http://example.com/37k.html -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/99.0.4844.51 Safari/537.36" -H "Accept: */*" -H "Connection: close"| grep -v 0,0,0 | tee -a 37k.txt &
 echo "91k"; wrk -s responses_code.lua --latency --timeout 60s -t 100 -c 650 -d 2m http://example.com/91k.html -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/99.0.4844.51 Safari/537.36" -H "Accept: */*" -H "Connection: close"| grep -v 0,0,0 | tee -a 91k.txt
+sleep 10
 echo "##########################################################################################"
-
-
 echo "################## HTTPS #################"
 echo "################## Run 135B #################"; wrk -s responses_code.lua --latency --timeout 60s -t 100 -c 2000 -d 2m https://example.com/135.html -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML,like Gecko) Chrome/99.0.4844.51 Safari/537.36" -H "Accept: */*" -H "Connection: close"| grep -v 0,0,0 | tee -a 135.txt &&
 sleep 2
